135eaa2eb1c839b0f1bebf8f014b74d1f431ccb5
William Hudson <mrbbaggins@gmail.com>
2011-08-06 23:26:19 -0400

    - Removed intro text * Cleaned up code
    Signed-off-by: William Hudson <mrbbaggins@gmail.com>

--- main.lua
+++ main.lua
@@ -1,28 +1,20 @@
--- global declaration
-
-fpsCap = 60
-
 function love.load()
+	screen_w = love.graphics.getWidth()
+	screen_h = love.graphics.getHeight()
 	love.graphics.setFont(24)	
 end
 
 function love.draw()
-	love.graphics.print("Zombie Game - Billy Hudson", 10, 10)
-	love.graphics.print("Press Enter to Start", 10, 40)
+	--love.graphics.print("Zombie Game", 300, 300)
 end
 
 function love.update(dt) -- dt is delta time (in seconds)
-	if fpsCap ~= 0 then
-		if dt < (1/fpsCap) then
-		  love.timer.sleep((1/fpsCap) - dt)
-		end
-	end
+	--love.timer.sleep(2000)
+	mapstate = love.filesystem.load("mapdraw.lua")
+	mapstate() -- this will overwrite all the functions in this file
+	love.load() -- call manually to relaod game state, will take effect next engine tick
 end
 
 function love.keypressed(key, unicode)
-	if key == "return" then
-		mapstate = love.filesystem.load("mapdraw.lua")
-		mapstate() -- this will overwrite all the functions in this file
-		love.load() -- call manually to relaod game state, will take effect next engine tick
-	end
+
 end
